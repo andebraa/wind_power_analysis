9ebd097b296178dff1868349e09557d195f37b33
anders <>
2021-07-07 08:02:03 +0200

    init search, scrape and reader commit

---
+++ datafile_reader.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import ast
+
+dictlist = []
+with open('havvind_2.txt') as file:
+    for elem in file:
+
+        line_dict = ast.literal_eval(elem)
+
+        dictlist.append(line_dict)
+
+
+
+df = pd.DataFrame(dictlist)
+print(df.columns)
+print(df['place'])
+places=df['place']
