d9bb0e25a990da48d98e877f138d5eef66771757
andebraa <andebraa@uio.no>
2021-09-29 14:20:38 +0000

    end of day

--- trainingdata_maker.py
+++ trainingdata_maker.py
@@ -4,7 +4,7 @@ import numpy as np
 data = pd.read_csv('full_geodata_longlat_noforeign.csv',
                     usecols = ['text'])
 
-tweet_indx = np.random.randint(0, len(data), size = 300) 
+tweet_indx = np.random.randint(0, len(data), size = 100) 
 
 
 trainingdata = data.iloc[tweet_indx, :] 
@@ -13,14 +13,18 @@ trainingdata = data.iloc[tweet_indx, :]
 pos_mask = np.zeros(len(trainingdata))
 
 for i, text in enumerate(trainingdata['text']):
-    print('-'*20)
+    print('-'*20, i)
     print(text)
-    arg = int(input('1: postivie, 0: negative  ')) 
+    try:
     
+        arg = int(input('1: postivie, 0: negative  ')) 
+    except:
+        print('argument must be int.')
+        arg = int(input('1: postivie, 0: negative  '))
     pos_mask[i] = int(arg)
 
 trainingdata['label'] = pos_mask.astype(int)
 
-trainingdata.to_csv('anotized_data.csv')
+trainingdata.to_csv('anotized_data_100.csv')
 
 
