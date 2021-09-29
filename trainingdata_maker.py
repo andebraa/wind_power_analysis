c1e0853a7bd49660bda5a61afafa678629fb1be6
andebraa <andebraa@uio.no>
2021-09-29 07:41:21 +0000

    trainingdata maker now works

--- trainingdata_maker.py
+++ trainingdata_maker.py
@@ -7,15 +7,19 @@ data = pd.read_csv('full_geodata_longlat_noforeign.csv',
 tweet_indx = np.random.randint(0, len(data), size = 300) 
 
 
-trainingdata = data.iloc[tweet_indx[298:], :]
-trainingdata['label'] = 0 
+trainingdata = data.iloc[tweet_indx, :] 
+
+
+pos_mask = np.zeros(len(trainingdata))
 
 for i, text in enumerate(trainingdata['text']):
     print('-'*20)
     print(text)
-    arg = input('1: postivie, 0: negative  ') 
+    arg = int(input('1: postivie, 0: negative  ')) 
     
-    trainingdata['label'][i] = arg
+    pos_mask[i] = int(arg)
+
+trainingdata['label'] = pos_mask.astype(int)
 
 trainingdata.to_csv('anotized_data.csv')
 
