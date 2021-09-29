9d434d879355a76cd29dfc8fe949b447326c8e8f
andebraa <andebraa@uio.no>
2021-09-29 06:11:09 +0000

    script for selecting 300 random tweets, and annotating them

---
+++ trainingdata_maker.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import numpy as np
+
+data = pd.read_csv('full_geodata_longlat_noforeign.csv',
+                    usecols = ['text'])
+
+tweet_indx = np.random.randint(0, len(data), size = 300) 
+
+
+trainingdata = data.iloc[tweet_indx[298:], :]
+trainingdata['label'] = 0 
+
+for i, text in enumerate(trainingdata['text']):
+    print('-'*20)
+    print(text)
+    arg = input('1: postivie, 0: negative  ') 
+    
+    trainingdata['label'][i] = arg
+
+trainingdata.to_csv('anotized_data.csv')
+
+
