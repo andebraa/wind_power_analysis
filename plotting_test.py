d31b472319f9e24aa91f74dc25f13748fc2d182d
andebraa <andebraa@uio.no>
2021-10-02 16:11:24 +0200

    merge

--- plotting_test.py
+++ plotting_test.py
@@ -2,14 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import geopandas
 
-<<<<<<< HEAD
 
 norway = geopandas.read_file('kommuner_komprimert.json')
-=======
-norway = geopandas.read_file('kommuner_komprimert.json')
-
-
->>>>>>> ec1da845b99d57894b82eeb0340942477fd335a3
 
 data = pd.read_csv('small_datafile.csv',
                   usecols = ['username','text','loc','created_at',
@@ -17,10 +11,7 @@ data = pd.read_csv('small_datafile.csv',
                              'longitude']
                   )
 
-<<<<<<< HEAD
-norway.plot()
-plt.plot(data['latitude'], data['longitude'])
-=======
+
 gdf = geopandas.GeoDataFrame(data, geometry=geopandas.points_from_xy(data.longitude, data.latitude))
 
 print(gdf.head())
@@ -30,7 +21,7 @@ norway = world.log[world['name'] == 'Norway']
 
 ax = world[world.continent == 'Europe'].plot()
 gdf.plot(ax=ax, color = 'red')
->>>>>>> ec1da845b99d57894b82eeb0340942477fd335a3
+
 plt.show()
 
 
