7162796cb498fa0aafc0e93ba1afc801e8327b79
andebraa <andebraa@uio.no>
2021-12-20 17:43:17 +0000

    added a third class to training data maker. 2: netural, 1 positive and 0 negative

--- trainingdata_maker.py
+++ trainingdata_maker.py
@@ -1,30 +1,50 @@
+"""
+Script for anotizing trainingdata.
+Currently has three categories, 2 for neutral, 1 for positive and 0 for negative.
+change the num_tweets variable to alter the number of tweets to annotate.
+
+"""
 import pandas as pd 
 import numpy as np
 
-data = pd.read_csv('full_geodata_longlat_noforeign.csv',
+data = pd.read_csv('data/full_geodata_longlat_noforeign.csv',
                     usecols = ['text'])
 
-tweet_indx = np.random.randint(0, len(data), size = 200) 
+output_file = 'test_anotate.csv'
+num_tweets = 5
 
-
+tweet_indx = np.random.randint(0, len(data), size = num_tweets) 
 trainingdata = data.iloc[tweet_indx, :] 
 
 
-pos_mask = np.zeros(len(trainingdata))
+label = np.zeros(len(trainingdata))
 
-for i, text in enumerate(trainingdata['text']):
-    print('-'*20, i)
-    print(text)
+def label_prompt():
+    
+    labels = [0,1,2]
     try:
     
-        arg = int(input('1: postivie, 0: negative  ')) 
+        arg = int(input('2: neutral/informative, 1: postivie, 0: negative  ')) 
+        assert int(arg) in labels
     except:
-        print('argument must be int.')
-        arg = int(input('1: postivie, 0: negative  '))
-    pos_mask[i] = int(arg)
+        print('argument must be int, and either 0, 1 or 2')
+        arg = int(input('2: neutral/informative, 1: postivie, 0: negative  '))
 
-trainingdata['label'] = pos_mask.astype(int)
+    return arg
 
-trainingdata.to_csv('anotized_data_100_6.csv', index=False)
+for i, text in enumerate(trainingdata['text']):
+    print('-'*20+'\n', i)
+    print(text)
+    
+
+    arg = label_prompt()
+        
+
+
+    label[i] = int(arg)
+
+
+trainingdata['label'] = label.astype(int)
+trainingdata.to_csv(output_file, index=False)
 
 
