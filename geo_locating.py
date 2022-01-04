d00c219dfa036ae02e262e0a4849496581e63a8b
andebraa <andebraa@uio.no>
2022-01-04 11:06:58 +0000

    further tweaks to geo_locating

--- geo_locating.py
+++ geo_locating.py
@@ -22,10 +22,18 @@ def geolocate(user_input):
         #add capitalization and other features
         #TODO manually handle cases such as 'Jorden' and 'Norge'?
         #TODO handle edge cases where user_input messes with regex pattern search
-        if places.str.contains(str(elem), case=False).any():
-            print(places[places.str.contains(str(elem), case=False)])
-            return True
-
+        potential_place = places.str.contains(str(elem), case=False)
+        if potential_place.any():
+            print('--------------------')
+            print('user_input',user_input)
+            print('potential place', potential_place)
+            pot_places = places[potential_place].tolist() 
+            print('potentail places', pot_places)
+            print('--------------------')
+            if len(pot_places) > 1:
+                return True, pot_places[0]
+            else:
+                return True, pot_places
 
 
 data = pd.read_csv('data/second_rendition_data/second_rendition_output.csv',
@@ -34,13 +42,22 @@ data = pd.read_csv('data/second_rendition_data/second_rendition_output.csv',
 
 orig_len = len(data)
 #extract rows that has a valid place name
+
+
+print('length before removing "norge" and "Norway', len(data))
+#we wish to remove the known larges occurences of locations, i.e. oslo, bergen etc
+data = data[data['loc'] != 'Norge']
+data = data[data['loc'] != 'Norway']
+
+print('length after removing norge and norway', len(data))
+
+
 cty=[]
 
 i = 0
 for i, line in enumerate(data['loc']):
     #extracting the place names that make sense, in string form
     print(line, i)
-    print('-------')
     if geolocate(line):
         cty.append(line)
     else:
@@ -59,6 +76,33 @@ print('original length: ', orig_len)
 print('len geodata: ', alt_len)
 print('percentage: ', orig_len/alt_len)
 
+
+
+#getting geografical data from nominatim. NOTE max 1 request per second
+nom_instance = Nominatim(user_agent = 'GetLoc')
+latitude = []
+longitude = []
+
+oslo_coords = nom_instance.geocode('Oslo, Norway') 
+bergen_coords = nom_instance.geocode('Bergen, Norway')
+trondheim_coords = nom_instance.geocode('Trondheim, Norway') 
+
+data_out.loc[data['loc'] == 'Oslo, Norway', 'latitude'] = oslo_coords.latitude  
+data_out.loc[data['loc'] == 'Oslo, Norway', 'longitude'] = oslo_coords.longitude
+data_out.loc[data['loc'] == 'Oslo, Norge', 'latitude'] = oslo_coords.latitude
+data_out.loc[data['loc'] == 'Oslo, Norge', 'longitude'] = oslo_coords.longitude
+data_out.loc[data['loc'] == 'Oslo', 'latitude'] = oslo_coords.latitude
+data_out.loc[data['loc'] == 'Oslo', 'longitude'] = oslo_coords.longitude
+data_out.loc[data['loc'] == 'Bergen, Norway', 'latitude'] = bergen_coords.latitude
+data_out.loc[data['loc'] == 'Bergen, Norway', 'longitude'] = bergen_coords.longitude
+data_out.loc[data['loc'] == 'Bergen', 'latitude'] = bergen_coords.latitude
+data_out.loc[data['loc'] == 'Bergen', 'longitude'] = bergen_coords.longitude
+data_out.loc[data['loc'] == 'Trondheim, Norway', 'latitude'] = trondheim_coords.latitude
+data_out.loc[data['loc'] == 'Trondheim, Norway', 'longitude'] = trondheim_coords.longitude
+data_out.loc[data['loc'] == 'Trondheim, Norge', 'latitude'] = trondheim_coords.latitude
+data_out.loc[data['loc'] == 'Trondheim, Norge', 'longitude'] = trondheim_coords.longitude
+
+#TODO: start loop over 1 second requests here
 
 print(data_out)
 data_out.to_csv('second_rendition_test_geolocate.csv')
