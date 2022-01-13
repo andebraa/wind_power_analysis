c79dca56d8aedebbc2e489a5db17cc40edfc4b52
andebraa <andebraa@uio.no>
2022-01-13 20:32:31 +0000

    some progress. issues with dropping useless places, and few tweets getting through

--- geo_locating.py
+++ geo_locating.py
@@ -81,7 +81,6 @@ def geolocate(user_input):
         try:
             elem = elem.replace(',', '')
         except:
-            print('pass')
             pass
         
         #try:
@@ -93,29 +92,22 @@ def geolocate(user_input):
         if potential_place_mask.any():
             pot_places = places_longlat.loc[potential_place_mask].astype('string')
             pot_places.index = pd.RangeIndex(len(pot_places.index)) #reset index of pot_places 
-            print('-'*10)
-            print('pot_places: \n',pot_places)
-            print('len pot places ', len(pot_places))            
-           
-            #if len(pot_places['places']) > 1: #more than one place matches 
-                
-
             
             if len(pot_places) > 1:
                 print('\n ******************************** \n ')
                 #If multiple places fulfill the sequence criteria we use
                 #SequenceMatcher to select the one which fits the best.
                 best_match, best_indx = ratio_finder(user_input, pot_places['places'].tolist())
-                print('after ratio finder')
-                print(best_match, best_indx)
                 best_long = pot_places.loc[best_indx, 'longitude']
                 best_lat = pot_places.loc[best_indx, 'latitude']
-                print(best_long)
-                print(best_lat)
-                return True, best_match, best_long, best_lat 
+
+                return True, best_match, float(best_long), float(best_lat) #best match is string 
             else:
-                return True, pot_places, places_longlat[potential_place_mask].longitude, \
-                             places_longlat[potential_place_mask].latitude
+                print('-----------else---------')
+                print(pot_places)
+                print(type(pot_places))
+                return True, pot_places.places.to_string(index=False), float(places_longlat[potential_place_mask].longitude), \
+                             float(places_longlat[potential_place_mask].latitude)
 
         else:
             print('lost case: ', elem)
@@ -140,22 +132,39 @@ data['latitude'] = ''
 data['longitude'] = ''
 
 drops = 0
+unexplained_drops = 0
+j = 0
 for i, line_ in data.iterrows():
     #extracting the place names that make sense, in string form
     line = line_['loc']
     print(i, line)    
-    #try:
-    bol, place, _longitude, _latitude = geolocate(line)
-    print('after call \n \n')
-    print('bol: \n', bol)
-    print('place: \n', place)
-    print('long: \n', _longitude)
-    print('lat: \n', _latitude)
+    try:
+        bol, _place_, _longitude, _latitude = geolocate(line)
+    except:
+        unexplained_drops += 1
+        data = data.drop(data.index[j])
+        
+        data.index = pd.RangeIndex(len(data.index)) #reset index of pot_places 
+        continue 
     
     if bol:
-        cty.append(place)
+
+        
+        print('\n \n after call')
+        print(type(_place_))
+        print(_place_)
+        #print('place: \n', _place_.places.to_string(index=False))
+        print('long: \n', _longitude)
+        print('lat: \n', _latitude)
+
+
+
+        print(type(_place_))
+        #cty.append(_place_['places'].to_string(index=False))
+        cty.append(_place_)
         longitude.append(_longitude)
         latitude.append(_latitude)
+        j += 1
 
     else:
         cty.append('drop')
@@ -163,127 +172,39 @@ for i, line_ in data.iterrows():
         latitude.append(0)
         print('drop; ', drops)
         drops += 1
+        j += 1
+
+
+print('drops: ', drops)
+print('unexplained drops: ', unexplained_drops)
+
 data['loc'] = cty
 data['latitude'] = latitude
 data['longitude'] = longitude 
 
-data = data.drop(data['loc'] == 'drop')
-data = data.drop(data['longitude'] == 0)
-data = data.drop(data['latitude'] == 0)
-
+print(data.head())
+print('here \n')
+print(data.tail())
+data.to_csv('test_data.csv')
+data = data.drop(data[data['loc'] != 'drop'].index)
+data = data.drop(data[data['latitude'] != 0].index)
+data = data.drop(data[data['longitude'] != 0].index)
 
 print('length after: ', len(data)) 
-print('drops: ', drops)
 data.to_csv('second_rendition_test_geolocate.csv')
-"""
-#note, data_out is now all the data with geo locations
-data_out = pd.DataFrame()
-
-data_out = data[data.city != ''].copy() #removing tweets without location
-alt_len = len(data_out) 
-print('original length: ', orig_len)
-print('len geodata: ', alt_len)
-print('percentage: ', alt_len/orig_len)
-
-print('-----------')
-print(len(data_out))
-print(len(data))
-
-
-data_out['indx'] = np.arange(len(data_out)) #manually make index collumn. fuck pandas
-data_out['indx'] = data_out['indx'].astype(int)
-data_out['latitude'] = ''
-data_out['longitude'] = '' #initialixing empty latitude longitude collumn
-
-print(data_out['latitude'])
-#removing mentions of Oslo and Bergen, as there are a fuck ton of them which takes time
-#working data is only used to place the non bergen oslo trondheim places on the map
-
-#woorking_data = data_out.mask((data_out['loc'] != 'Oslo, Norge')|(data_out['loc'] != 'Oslo, Norway')|\
-#                              (data_out['loc'] != 'Bergen, Norway') | (data_out['loc'] != 'Oslo') |\
-#                              (data_out['loc'] != 'Bergen') | (data_out['loc'] != 'Trondheim, Norge') |\
-#                              (data_out['loc'] != 'Trondheim, Norway')) 
-
-
-working_data = data_out[data_out['loc'] != 'Oslo, Norge']
-working_data = working_data[working_data['loc'] != 'Oslo, Norway']
-working_data = working_data[working_data['loc'] != 'Bergen, Norway']
-working_data = working_data[working_data['loc'] != 'Oslo']
-working_data = working_data[working_data['loc'] != 'Bergen']
-working_data = working_data[working_data['loc'] != 'Trondheim, Norge']
-working_data = working_data[working_data['loc'] != 'Trondheim, Norway']
-#this reduces from 44423 to 14152
 
 
 
-#getting geografical data from nominatim. NOTE max 1 request per second
-nom_instance = Nominatim(user_agent = 'GetLoc')
-
-oslo_coords = nom_instance.geocode('Oslo, Norway') 
-bergen_coords = nom_instance.geocode('Bergen, Norway')
-trondheim_coords = nom_instance.geocode('Trondheim, Norway') 
-
-data_out.loc[data_out['loc'] == 'Oslo, Norway', 'latitude'] = oslo_coords.latitude  
-data_out.loc[data_out['loc'] == 'Oslo, Norway', 'longitude'] = oslo_coords.longitude
-data_out.loc[data_out['loc'] == 'Oslo, Norge', 'latitude'] = oslo_coords.latitude
-data_out.loc[data_out['loc'] == 'Oslo, Norge', 'longitude'] = oslo_coords.longitude
-data_out.loc[data_out['loc'] == 'Oslo', 'latitude'] = oslo_coords.latitude
-data_out.loc[data_out['loc'] == 'Oslo', 'longitude'] = oslo_coords.longitude
-data_out.loc[data_out['loc'] == 'Bergen, Norway', 'latitude'] = bergen_coords.latitude
-data_out.loc[data_out['loc'] == 'Bergen, Norway', 'longitude'] = bergen_coords.longitude
-data_out.loc[data_out['loc'] == 'Bergen', 'latitude'] = bergen_coords.latitude
-data_out.loc[data_out['loc'] == 'Bergen', 'longitude'] = bergen_coords.longitude
-data_out.loc[data_out['loc'] == 'Trondheim, Norway', 'latitude'] = trondheim_coords.latitude
-data_out.loc[data_out['loc'] == 'Trondheim, Norway', 'longitude'] = trondheim_coords.longitude
-data_out.loc[data_out['loc'] == 'Trondheim, Norge', 'latitude'] = trondheim_coords.latitude
-data_out.loc[data_out['loc'] == 'Trondheim, Norge', 'longitude'] = trondheim_coords.longitude
-
-print(data_out['latitude'])
-
-i = 0
-
-fin = len(working_data['city'])
-
-for i, row in working_data.iterrows():
-    place = row['city']
-    if isinstance(place, list):
-        place = place[0] 
-    print(place)
-    print(i, fin)
-    if place in places_longlat['places']
-    if country[-5:] != 'Norge': #Shouldn't be any occurances of this
-        print('-'*20)
-        #data_out.iloc[data_out.index[workdata_indx], 'latitude'] = 'drop'
-        data_out.loc[workdata_indx, 'latitude'] = 'drop'
-        data_out.loc[workdata_indx, 'longitude'] = 'drop'
-    else:
-
-        print(workdata_indx)
-        print(type(workdata_indx))
-        data_out.loc[workdata_indx, 'latitude'] = getLoc.latitude
-        data_out.loc[workdata_indx, 'longitude'] = getLoc.longitude
-    time.sleep(1) 
-    
-    i += 1
 
 
 
-#for i in range(len(working_data['city'])):
-#    latitude.append(np.random.randint(1000))
-#    longitude.append(np.random.randint(1000))
-#print(len(latitude))
-#print(data_out.latitude.value_counts())
-data_out[~data_out.latitude.str.contains('drop')]
-data_out[~data_out.longitude.str.contains('drop')]
 
-#data_out.loc[data_out['latitude'] == '', 'latitude'] = latitude
-#data_out.loc[data_out['longitude'] == '', 'longitude'] = longitude
-#data_out = data_out[data_out['latitude'] != ''] 
 
-print('size after nominatim: ', len(data_out))
 
-del data_out['indx']
 
-print(data_out)
-data_out.to_csv('second_rendition_test_geolocate.csv')
-"""
+
+
+
+
+
+
