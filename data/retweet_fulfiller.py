36560faf6e196bb6bd92e60a61031aadf301b922
andebraa <andebraa@uio.no>
2022-01-17 11:50:25 +0000

    in retweet fulfiller, the original tweet might not occur in dataset. implemented a fix for these. they are skipped

--- data/retweet_fulfiller.py
+++ data/retweet_fulfiller.py
@@ -36,21 +36,33 @@ def fulfill_retweets(filename):
         uname = res[0][0]
         stripped_string = res[0][1]
         print('-------------------')
+        print('stripped string :\n', stripped_string)
+        print(uname)
+        
+        #Some original tweets might not occur in the dataset (user might not have geo tag, or 
+        # not directly mention wind energy). if so we skip to the next one
 
+        user_tweets = data.loc[data['username']==uname] 
+        print(user_tweets)
+        if user_tweets.empty:
+            continue #skips to next iteration of loo#skips to next iteration of loopp
+        stop
         #finding the original tweet based on the stripped retweet
         for _i, _row in non_retweets.iterrows():
-            print('\nRT ', row['text'])
-            print('\noriginal',_row['text'])
+            #print('\nRT ', row['text'])
+            #print('\noriginal',_row['text'])
             preamble = 5 + len(uname) +1 #RT @<uname>:
             #print(row['text'][preamble:preamble+len(stripped_string)])
             #print(_row['text'][preamble:preamble+len(stripped_string)])
 
             #print(row['text'][preamble:preamble+len(stripped_string)] == _row['text'][preamble:preamble+len(stripped_string)])
+            if _row.str.contains(stripped_string, regex=False).any():
+                print('twat')
             if re.findall(re.escape(stripped_string), _row['text']):
                 print('match')
         match_case = non_retweets[non_retweets['text'].str.contains(re.escape(stripped_string), na=False)==True]
-        #match_mask = non_retweets['text'].str.contains(stripped_string)
-        #print(non_retweets[match_mask])
+        match_mask = non_retweets['text'].str.contains(stripped_string)
+        print(non_retweets[match_mask])
         #match_case = non_retweets[match_mask]
         print('original tweet: \n', match_case)
         #setting the text row in dataset to be the original tweet
@@ -59,8 +71,8 @@ def fulfill_retweets(filename):
         print('--------w----------')
 #        print(data.iloc[row['indx']]['text'])
 
-        if itera == 2:
-            break
+        if itera == 5:
+            return 0
         itera += 1
     data.to_csv('no_unfulfilled_retweets.csv')
 
