aae2bcf6097966f80d11d2b64549b487f5d7c25f
andebraa <andebraa@uio.no>
2022-05-06 13:12:44 +0000

    anon_idetifyer has function for turning anon usernames back into original

--- data/anon_identifier.py
+++ data/anon_identifier.py
@@ -9,7 +9,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 from matplotlib.patches import Rectangle
 
-data = pd.read_csv('third_rendition_data/third_rendition_output.csv', parse_dates = True)# , usecols = [''])
+data = pd.read_csv('third_rendition_data/third_rendition_geolocated.csv', parse_dates = True)# , usecols = [''])
 ID = {} #dictionary to translate ID and usernames
 tweet_occurances = {} #keep track of number of tweets per user
 
@@ -24,52 +24,71 @@ def generate_ID(ID):
     else:
         return generate_ID(ID)
 
-for i, elem in enumerate(data['username']):
-    #If the element (username) has already been given a anon value, 
-    if elem in ID.values():
-        #this nasty line extracts the key given a value. apparently it's the only way
-        #might have issues if lists get too long
-        new_ID = list(ID.keys())[list(ID.values()).index(elem)]
-        tweet_occurances[elem] += 1 #another tweet from user elem
-    else:
-        new_ID = generate_ID(ID)
-        assert not np.isnan(new_ID)
-        tweet_occurances[elem] = 1 #first tweet by user elem 
-    ID[new_ID] = elem
-    data.loc[i, 'username'] = new_ID
-    #data['username'][i] = new_ID
+def anonymizer():
+    for i, elem in enumerate(data['username']):
+        #If the element (username) has already been given a anon value, 
+        if elem in ID.values():
+            #this nasty line extracts the key given a value. apparently it's the only way
+            #might have issues if lists get too long
+            new_ID = list(ID.keys())[list(ID.values()).index(elem)]
+            tweet_occurances[elem] += 1 #another tweet from user elem
+        else:
+            new_ID = generate_ID(ID)
+            assert not np.isnan(new_ID)
+            tweet_occurances[elem] = 1 #first tweet by user elem 
+        ID[new_ID] = elem
+        data.loc[i, 'username'] = new_ID
+        #data['username'][i] = new_ID
 
-print("number of unique users: {}".format(len(ID)))
+    print("number of unique users: {}".format(len(ID)))
 
-max_number_of_tweets = max(tweet_occurances.values())
-num_of_tweets = np.arange(1, max_number_of_tweets+2) 
-for elem in tweet_occurances.values():
-    num_of_tweets[elem] += 1 
-
-
-print("highest number of tweets by single user:{}".format(max_number_of_tweets)) 
-fig, ax = plt.subplots()
-
-for label in (ax.get_xticklabels() + ax.get_yticklabels()):
-    label.set_fontsize(8)
+    max_number_of_tweets = max(tweet_occurances.values())
+    num_of_tweets = np.arange(1, max_number_of_tweets+2) 
+    for elem in tweet_occurances.values():
+        num_of_tweets[elem] += 1 
 
 
-#logbins=np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
+    print("highest number of tweets by single user:{}".format(max_number_of_tweets)) 
+    fig, ax = plt.subplots()
 
-max_key = max(tweet_occurances, key=tweet_occurances.get)
-print(max_key)
-print(tweet_occurances[max_key]) 
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        label.set_fontsize(8)
 
-ax.hist(tweet_occurances.values() , bins = np.linspace(1, max_number_of_tweets-2, 300))
-plt.yscale('log')
-plt.xscale('linear')
-plt.ylabel('number of users')
-plt.xlabel('number of tweets')
-plt.title('Frequency of tweets by users. 2007 -2021')
 
-plt.savefig('third_rendition_data/third_rendition_output_user_tweetfreq.png',dpi = 300, format='png',  bbox_inches = 'tight', pad_inches = 0.1) #0.1 is default when bbox is tight
+    #logbins=np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
 
-#outfile_ID = open('second_rendition_data/second_rendition_geolocated_noemoji_translator.json', 'w')
-#json.dump(ID, outfile_ID) #writing the translation dictionary to file
+    max_key = max(tweet_occurances, key=tweet_occurances.get)
+    print(max_key)
+    print(tweet_occurances[max_key]) 
 
-#data.to_csv('third_rendition_data/third_rendition_geolocated_output_anonymous.csv', index=False)
+    ax.hist(tweet_occurances.values() , bins = np.linspace(1, max_number_of_tweets-2, 300))
+    plt.yscale('log')
+    plt.xscale('linear')
+    plt.ylabel('number of users')
+    plt.xlabel('number of tweets')
+    plt.title('Frequency of tweets by users. 2007 -2021')
+
+    plt.savefig('third_rendition_data/third_rendition_geolocated_user_tweetfreq.png',dpi = 300, format='png',  bbox_inches = 'tight', pad_inches = 0.1) #0.1 is default when bbox is tight
+
+    outfile_ID = open('third_rendition_data/third_rendition_geolocated_translator.json', 'w')
+    json.dump(ID, outfile_ID) #writing the translation dictionary to file
+
+    data.to_csv('third_rendition_data/third_rendition_geolocated_anonymous.csv', index=False)
+
+def identifyer():
+    df = pd.read_csv('third_rendition_data/third_rendition_geolocated_anonymous_posneutral_predict.csv')
+
+    with open('third_rendition_data/third_rendition_geolocated_translator.json') as fp:
+        trans_dict = json.loads(fp.read())
+
+    
+    for i, elem in df.iterrows():
+        elem = elem['username']
+        
+        
+        df.loc[i, 'username'] = trans_dict[str(elem)]
+    print(df)
+    df.to_csv('third_rendition_data/third_rendition_geolocated_posneutral_predict.csv')
+        
+if __name__ == '__main__':
+    identifyer()
